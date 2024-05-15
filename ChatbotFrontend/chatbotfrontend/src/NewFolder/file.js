b9a929719a8f32aad705e64362e72b324d24ff14
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2024-05-15 17:38:56 +0200

    Update

--- ChatbotFrontend/chatbotfrontend/src/NewFolder/file.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('enterButton').addEventListener('click', function () {
-    alert('Enter button clicked with values: ' + document.getElementById('textbox1').value + ' and ' + document.getElementById('textbox2').value);
-});
-
-document.getElementById('stopButton').addEventListener('click', function () {
-    document.getElementById('textbox1').value = '';
-    document.getElementById('textbox2').value = '';
-    alert('Inputs cleared!');
-});
