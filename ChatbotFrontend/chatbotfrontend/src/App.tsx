54d8c7f5b232ddb71a90e7db8bffaa78bb08f917
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2024-05-16 20:00:54 +0200

    Made components

--- ChatbotFrontend/chatbotfrontend/src/App.tsx
+++ ChatbotFrontend/chatbotfrontend/src/App.tsx
@@ -2,51 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Chat from './Components/Chat';
 
 function App() {
-    const [inputOne, setInputOne] = useState('');
-    const [response, setResponse] = useState('');
-
-    const handleInputOneChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-        setInputOne(event.target.value);
-    };
-
-    const handleInputTwoChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-        setResponse(event.target.value);
-    };
-
-    const handleEnter = () => {
-        console.log('Enter clicked!');
-        console.log('Input One:', inputOne);
-        console.log('Input Two:', response);
-    };
-
-    const handleStop = () => {
-        console.log('Stop clicked!');
-        setInputOne('');
-        setResponse('');
-    };
-
     return (
         <div>
-            <input
-                type="text"
-                value={inputOne}
-                onChange={handleInputOneChange}
-                placeholder="How can I help you?"
-            />
-            <button onClick={handleEnter}>Enter</button>
-            <input
-                type="text"
-                value={response}
-                onChange={handleInputTwoChange}
-                placeholder="Response"
-                readOnly
-            />
-            <br></br>
-            <button onClick={handleStop}>Stop</button>
+            <Chat message={"Test"} user={"me"} />
         </div>
-    );
+    )
 }
 
 export default App;
